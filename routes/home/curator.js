92ef894d6ba7a9db049c263d7a255a80ff7689fd
jungahshin <shinasong@naver.com>
2019-08-22 03:02:44 +0900

    home/curator 추가

--- routes/home/curator.js
+++ routes/home/curator.js
@@ -1,4 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../../module/pool');
+const util = require('../../module/utils');
+const statusCode = require('../../module/statusCode');
+const resMessage = require('../../module/responseMessage');
+
+router.get('/:curatorIdx', async(req, res) => {
+    try{
+        const curator = req.params.curatorIdx;
+        const getCuratorQuery = 'SELECT * FROM curator WHERE curatorIdx = ?';
+        const getCuratorResult = await db.queryParam_Parse(getCuratorQuery, [curator]);
+
+            if (getCuratorResult == 0) {
+                res.status(200).send(util.successFalse(statusCode.DB_ERROR, resMessage.CURATOR_SELECT_FAIL));
+            } else {
+                res.status(200).send(util.successTrue(statusCode.OK, resMessage.CURATOR_SELECT_SUCCESS,getCuratorResult));
+                console.log(getCuratorResult);
+            }
+        }catch(err){
+            console.log(err);
+        }
+});
+
 module.exports = router;
