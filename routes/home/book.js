687f7653583d8490936e58537770ca1ef6a24749
jungahshin <shinasong@naver.com>
2019-08-24 01:34:56 +0900

    home/book 최종 완성

--- routes/home/book.js
+++ routes/home/book.js
@@ -7,8 +7,9 @@ const statusCode = require('../../module/statusCode');
 const resMessage = require('../../module/responseMessage');
 
 // home/search/book?query=검색어
+//author_intro없어,,,
 
-router.get('/search/book', function (req, res) {
+router.get('/search/book', function(req, res) {
     var request = require('request');
     var options = {
         display : 1
@@ -21,10 +22,33 @@ router.get('/search/book', function (req, res) {
           'X-Naver-Client-Id':secret_config.federation.naver.client_id,
           'X-Naver-Client-Secret':secret_config.federation.naver.client_secret
         }
-      }, function(error, response, body) {
+      }, async function(error, response, body) {
             if (!error && response.statusCode == 200) {
-                res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-                res.end(body);
+                // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                // // res.end(body);
+                var json = JSON.parse(body);
+                var title_tag = (json['items'][0])['title']//title에 tag있는 상태
+                var title = title_tag.replace(/(<b>|<\/b>)/gi, "");//title에 tag없는 상태
+                var book_image = (json['items'][0])['image'];
+                var author = (json['items'][0])['author'];
+                var summary_tag = (json['items'][0])['description'];
+                var summary = summary_tag.replace(/(<b>|<\/b>)/gi, "");
+                var publisher = (json['items'][0])['publisher'];
+                const insertBookQuery = 'INSERT INTO book (title, publisher, author, summary, book_image) VALUES (?, ?, ?, ?, ?)';
+                const insertBookResult = await db.queryParam_Parse(insertBookQuery, [title , publisher, author, summary, book_image]);
+                const book_result=[];
+                var book_json = new Object();
+                book_json.title = title;
+                book_json.publisher = publisher;
+                book_json.author = author;
+                book_json.summary = summary;
+                book_json.book_image = book_image;
+                book_result.push(book_json);
+                if(insertBookResult.length == 0){//실패
+                    res.status(200).send(util.successFalse(statusCode.OK, resMessage.BOOK_INSERT_FAIL));
+                }else{
+                    res.status(200).send(util.successTrue(statusCode.OK, resMessage.BOOK_INSERT_SUCCESS, book_result));
+                }
             } else {
                 res.status(response.statusCode).end();
                 console.log('error = ' + response.statusCode);
