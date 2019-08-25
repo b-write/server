4fa2ad788b23df1bcf151e1cf851ed213ec4ee8f
jungahshin <shinasong@naver.com>
2019-08-26 02:48:06 +0900

    git 충돌 정리

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/home', require('./home'));
-<<<<<<< HEAD
 router.use('/write', require('./write'));
-=======
 router.use('/book', require('./book'));
 router.use('/user', require('./user'));
->>>>>>> b255b7ac2ed0dc3ecf4d6188dd493c2376ee469c
 
 module.exports = router;
