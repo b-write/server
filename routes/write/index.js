335b418fc4194751b899dc4f7ec429bc34a68f83
hwangbosoyeon <soy2on45@naver.com>
2019-08-25 19:17:59 +0900

    [추가] write폴더 코드 추가

--- routes/write/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/answer', require('./answer'));
-router.use('/question', require('./question'));
-router.use('/myQuestionList', require('./myQuestionList'));
-router.use('/questionList', require('./questionList'))
-
-module.exports = router;
