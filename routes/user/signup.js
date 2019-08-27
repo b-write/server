245666521a85aa563702b9458d179eaf1f8761b5
jungahshin <shinasong@naver.com>
2019-08-27 22:38:18 +0900

    회원 가입/로그인 id 없앰

--- routes/user/signup.js
+++ routes/user/signup.js
@@ -7,17 +7,17 @@ const resMessage = require('../../module/responseMessage');
 const db = require('../../module/pool');
 const upload = require('../../config/multer');
 
-//body-name, id, pw, email, user_image
+//body-name(닉네임), pw, email(id 역할), user_image
 router.post('/', upload.single('user_image'), async (req, res) => {
     const selectUserQuery = 'SELECT * FROM user WHERE id = ?'
     const selectUserResult = await db.queryParam_Parse(selectUserQuery, [req.body.id]);
-    const signupQuery = 'INSERT INTO user (name, id, pw, salt, email, user_image) VALUES (?, ?, ?, ?, ?, ?)';
+    const signupQuery = 'INSERT INTO user (name, pw, salt, email, user_image) VALUES (?, ?, ?, ?, ?)';
     if(selectUserResult.length == 0) { //id 중복 없음(회원 가입 성공)
         if(req.file == undefined){//이미지 없음
             const buf= await crypto.randomBytes(64);
             const salt = buf.toString('base64');
             const hashedPw = await crypto.pbkdf2(req.body.pw.toString(), salt, 1000, 32, 'SHA512');
-            const signupResult = await db.queryParam_Parse(signupQuery, [req.body.name, req.body.id, hashedPw.toString('base64'), salt, req.body.email, null]);
+            const signupResult = await db.queryParam_Parse(signupQuery, [req.body.name, hashedPw.toString('base64'), salt, req.body.email, null]);
             if (signupResult.length == 0) {
                 res.status(200).send(util.successFalse(statusCode.DB_ERROR, resMessage.USER_INSERT_FAIL));
             } else {  
@@ -27,7 +27,7 @@ router.post('/', upload.single('user_image'), async (req, res) => {
             const buf = await crypto.randomBytes(64);
             const salt = buf.toString('base64');
             const hashedPw = await crypto.pbkdf2(req.body.pw.toString(), salt, 1000, 32, 'SHA512');
-            const signupResult = await db.queryParam_Parse(signupQuery, [req.body.name, req.body.id, hashedPw.toString('base64'), salt, req.body.email, user_image]);
+            const signupResult = await db.queryParam_Parse(signupQuery, [req.body.name, hashedPw.toString('base64'), salt, req.body.email, user_image]);
             if (signupResult.length == 0) {
                 res.status(200).send(util.successFalse(statusCode.DB_ERROR, resMessage.USER_INSERT_FAIL));
             } else {  
