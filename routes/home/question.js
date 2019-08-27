b511392c2de8d76d607373dd7202ecfcc5527507
jungahshin <shinasong@naver.com>
2019-08-27 22:39:59 +0900

    home/question 추가

--- routes/home/question.js
+++ routes/home/question.js
@@ -1,8 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../../module/pool');
+const util = require('../../module/utils');
+const statusCode = require('../../module/statusCode');
+const resMessage = require('../../module/responseMessage');
 
-//메인에서 현재 가장 인기있는 질문들을 누르면 어디 페이지로 넘어가나?
-//book_imgage, question_content, id , question_likes
+//questionIdx에 해당하는 bookIdx의 title, author, question_content, book_image, question_date
+//questionIdx에 해당하는 답변의 수 count(*)
+//qestionIdx에 해당하는 answer_content, user_image, name(닉네임 역할), answer_likes, answer_date
 
+//body-questinIdx를 받아서->defualt 좋아요 순
+router.post('/', async (req, res) => {
+    const getQuestionQuery = 'SELECT title, author, question_content, book_image, question_date FROM question JOIN book ON question.bookIdx = book.bookIdx WHERE questionIdx = ?';
+    const getAnswerQuery = 'SELECT answer_content, answer_likes, answer_date FROM answer JOIN question ON answer.questionIdx = question.questionIdx WHERE answer.questionIdx = ? ORDER BY answer_likes desc';
+    const getAnswerNumQuery = 'SELECT count(*) FROM answer WHERE questionIdx = ?';
+
+    const selectTransaction = await db.Transaction(async(connection) => {
+        getQuestionResult = await db.queryParam_Parse(getQuestionQuery, [req.body.questionIdx]);
+        getAnswerResult = await db.queryParam_Parse(getAnswerQuery, [req.body.questionIdx]);
+        getAnswerNumResult = await db.queryParam_Parse(getAnswerNumQuery, [req.body.questionIdx]);
+    });
+
+    if(selectTransaction == 0){//트랜잭션 실패
+        res.status(200).send(util.successFalse(statusCode.OK, resMessage.QUESTION_LOAD_FAIL));
+    }else{//트랜잭션 성공
+        getQuestionResult[0]['answer_num'] = getAnswerNumResult[0]['count(*)'];
+        const question_result=[];
+        question_result.push(getQuestionResult[0]);
+        for(var i=0; i<getAnswerResult.length; i++){
+            question_result.push(getAnswerResult[i]);
+        }
+        res.status(200).send(util.successTrue(statusCode.OK, resMessage.QUESTION_LOAD_SUCCESS, question_result));
+    }
+});
+
+//body-questinIdx를 받아서->최신순
+router.post('/latest', async (req, res) => {
+    const getAnswerQuery = 'SELECT answer_content, answer_likes, answer_date FROM answer JOIN question ON answer.questionIdx = question.questionIdx WHERE answer.questionIdx = ? ORDER BY answer_date desc';
+    getAnswerResult = await db.queryParam_Parse(getAnswerQuery, [req.body.questionIdx]);
+
+    if(getAnswerResult.length == 0){//트랜잭션 실패
+        res.status(200).send(util.successFalse(statusCode.OK, resMessage.QUESTION_LOAD_FAIL));
+    }else{//트랜잭션 성공
+        res.status(200).send(util.successTrue(statusCode.OK, resMessage.QUESTION_LOAD_SUCCESS, getAnswerResult));
+    }
+});
 
 module.exports = router;
