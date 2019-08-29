321fb3a0e88106fc14de7502efeca6911a3c064d
jungahshin <shinasong@naver.com>
2019-08-30 00:14:59 +0900

    메인 홈 수정

--- routes/home/main.js
+++ routes/home/main.js
@@ -13,7 +13,7 @@ const resMessage = require('../../module/responseMessage');
 //ORDER BY RAND() LIMIT 3
 
 router.get('/', async(req, res)=>{
-    const getBookQuery = 'SELECT bookIdx, title, author, book_image FROM book ORDER BY RAND() LIMIT 6';//3개 랜덤 추출
+    const getBookQuery = 'SELECT bookIdx, title, author, book_image FROM book WHERE bookIdx IN (17, 49, 50, 25, 30, 43)';//3개 랜덤 추출
     const getCuratorQuery = 'SELECT name, job, curator_image, curator_intro, book_image FROM curator JOIN book ON curator.bookIdx = book.bookIdx';//메인에서 책은 이미지만 필요
     const getQuestionQuery = 'SELECT questionIdx, question_content, question_likes, book_image FROM question JOIN book ON question.bookIdx = book.bookIdx ORDER BY question_likes DESC LIMIT 3';//질문 좋아요 수 상위3개 추출
 
@@ -25,11 +25,24 @@ router.get('/', async(req, res)=>{
     if(selectTransaction == 0){//트랜잭션 실패
         res.status(200).send(util.successFalse(statusCode.OK, resMessage.MAIN_FAIL));
     }else{//트랜잭션 성공
-        const main_result=[];
-        main_result.push(getBookResult);
-        main_result.push(getCuratorResult);
-        main_result.push(getQuestionResult);
-        res.status(200).send(util.successTrue(statusCode.OK, resMessage.MAIN_SUCCESS, main_result));
+        var main_json = new Object();
+        const book = [];
+        const curator = [];
+        const question = [];
+        for(var i=0; i< getBookResult.length; i++){
+            book.push(getBookResult[i]);
+        }
+        main_json.book = book;
+        for(var i=0; i< getCuratorResult.length; i++){
+            curator.push(getCuratorResult[i]);
+        }
+        main_json.curator = curator;
+        for(var i=0; i< getQuestionResult.length; i++){
+            question.push(getQuestionResult[i]);
+        }
+        main_json.question = question;
+
+        res.status(200).send(util.successTrue(statusCode.OK, resMessage.MAIN_SUCCESS, main_json));
     }
 });
 
